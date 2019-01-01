f057ded4bc1a0f616c7929112ca6a01ca6edd2d4
Leandro <contato@leandroaraujo.me>
2019-01-01 14:29:43 -0300

    chore(storybook): Add sample Alert with content.

--- packages/core/stories/Alert.stories.js
+++ packages/core/stories/Alert.stories.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 
-import { Alert, View } from '~/components';
+import {
+  Alert, Divider, Text, View,
+} from '~/components';
 import Decorator, { monospace } from './Decorator';
 
 const componentInformations = `The ${monospace('Alert')}.`;
@@ -15,5 +17,20 @@ storiesOf('Alert', module)
       <Alert type="success">This is a success example error!</Alert>
       <Alert type="warning">This is a warning example error!</Alert>
       <Alert type="danger">This is a danger example error!</Alert>
+    </View>
+  ))
+  .add('with content', () => (
+    <View>
+      <Alert type="success">
+        <Text type="h5">Complete Mission!</Text>
+        <Text>
+          This example text is going to run a bit longer so that you can see how spacing within an
+          alert works with this kind of content.
+        </Text>
+        <Divider />
+        <Text>
+          Whenever you need to, be sure to use margin utilities to keep things nice and tidy.
+        </Text>
+      </Alert>
     </View>
   ));
